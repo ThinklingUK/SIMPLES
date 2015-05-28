3e3ebbcde4ea9036f031b4234387fc97df39e650
Ellison <ellison.linsley@gmail.com>
2015-05-29 00:16:25 +0100

    check in

--- app/src/main/java/uk/thinkling/simples/MainActivity.java
+++ app/src/main/java/uk/thinkling/simples/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends ActionBarActivity {
 
 
     @Override
-    // Create the top bar menu  options
+    // Create the top bar menu options
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
