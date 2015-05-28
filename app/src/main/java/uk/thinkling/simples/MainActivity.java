96ea6fdea77274d4bb2af641e715ab7d2952331a
Ellison <ellison.linsley@gmail.com>
2015-05-28 23:54:30 +0100

    check in

---
+++ app/src/main/java/uk/thinkling/simples/MainActivity.java
@@ -0,0 +1,83 @@
+package uk.thinkling.simples;
+
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+
+    DrawView myDrawView;
+    Handler mHandler;
+
+    public TextView ScoreText,TimeLeftText,HighScoreText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // myDrawView = new DrawVision(this);
+        //setContentView(myDrawView);
+
+
+        ScoreText = (TextView) findViewById(R.id.ScoreView);
+        TimeLeftText = (TextView) findViewById(R.id.TimeLeftView);
+        HighScoreText = (TextView) findViewById(R.id.HighScoreText);
+
+
+        myDrawView = (DrawView) findViewById(R.id.drawView);
+
+
+
+        // start the timer handler that will invalidate the view
+        mHandler = new Handler();
+        mHandler.postDelayed(mRunnable, 1000);
+    }
+
+    // this is the runnable action that is called by the handler - it will add itself to the handler again after delay
+    private Runnable mRunnable = new Runnable() {
+
+        @Override
+        public void run() {
+            /** invalidate the view - forcing it to redraw **/
+            myDrawView.invalidate();
+
+            //do this again in 40 milliseconds - ie. 1/25th of a second
+            mHandler.postDelayed(mRunnable, 40);
+        }
+    };
+
+
+    @Override
+    // Create the top bar menu options
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    //handle the clicks on the top bar menu options
+        public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
